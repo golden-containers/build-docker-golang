7dfa05127a167db16c4a59e990ce9c5daee3b225
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-11-12 09:19:46 -0800

    Use label flag

--- build.sh
+++ build.sh
@@ -13,10 +13,11 @@ cd golang
 # Transform
 
 sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye-scm/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye-scm/" 1.17/bullseye/Dockerfile
-echo "LABEL ${1:-DEBUG=TRUE}" >> 1.17/bullseye/Dockerfile
 
 # Build
 
-docker build --tag ghcr.io/golden-containers/golang:1.17-bullseye 1.17/bullseye
+docker build 1.17/bullseye/ --tag ghcr.io/golden-containers/golang:1.17-bullseye --label ${1:-DEBUG=TRUE}
+
+# Push
 
 docker push ghcr.io/golden-containers/golang -a
