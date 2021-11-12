fd69d0c47be53a62d521fb6e479ce303e3699266
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-11-12 11:19:17 -0800

    Add explicit platform

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye-
 
 # Build
 
-docker build 1.17/bullseye/ --tag ghcr.io/golden-containers/golang:1.17-bullseye --label ${1:-DEBUG=TRUE}
+docker build 1.17/bullseye/ --platform linux/amd64 --tag ghcr.io/golden-containers/golang:1.17-bullseye --label ${1:-DEBUG=TRUE}
 
 # Push
 
