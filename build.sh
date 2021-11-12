7db775680628402da3fea54f171af8f3cddbcdde
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-11-12 12:57:47 -0800

    Refactor

--- build.sh
+++ build.sh
@@ -13,14 +13,23 @@ cd golang
 # Transform
 
 # This sed syntax is GNU sed specific
-# [ -z $(command -v gsed) ] && GNU_SED=sed || GNU_SED=gsed
+[ -z $(command -v gsed) ] && GNU_SED=sed || GNU_SED=gsed
 
-sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye-scm/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye-scm/" 1.17/bullseye/Dockerfile
+${GNU_SED} -i \
+    -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye-scm/; t" \
+    -e "1,// s//FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye-scm/" \
+    1.17/bullseye/Dockerfile
 
 # Build
 
-docker build 1.17/bullseye/ --platform linux/amd64 --tag ghcr.io/golden-containers/golang:1.17-bullseye --label ${1:-DEBUG=TRUE}
+[ -z "${1:-}" ] && BUILD_LABEL_ARG="" || BUILD_LABEL_ARG=" --label \"${1}\" "
+
+BUILD_PLATFORM=" --platform linux/amd64 "
+GCI_URL="ghcr.io/golden-containers"
+BUILD_ARGS=" ${BUILD_LABEL_ARG} ${BUILD_PLATFORM} "
+
+docker build 1.17/bullseye/ --tag ${GCI_URL}/golang:1.17-bullseye ${BUILD_ARGS}
 
 # Push
 
-docker push ghcr.io/golden-containers/golang -a
+docker push ${GCI_URL}/golang -a
